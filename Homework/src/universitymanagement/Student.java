41c507e56d6e7117e2362d7c1d06991563de00de
narendra81000191 <Narendra.Tomar@Coforge.com>
2026-02-27 23:26:52 +0530

    day2 assignment

---
+++ Homework/src/universitymanagement/Student.java
@@ -0,0 +1,19 @@
+package universitymanagement;
+
+public class Student {
+	String studentid;
+	String name;
+	int attendanceper;
+	public Student() {
+		super();
+	}
+	public Student(String studentid, String name, int attendanceper) {
+		super();
+		this.studentid = studentid;
+		this.name = name;
+		this.attendanceper = attendanceper;
+	}
+	
+	
+
+}
